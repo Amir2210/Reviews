453b504efe77695a62c38f3eaa2000585621054a
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-02-11 19:09:03 +0200

    Implemt onPrevPerson func

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,22 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 import data from './data'
 const App = () => {
   const [users, setUsers] = useState(null)
-  const [index, setIndex] = [0]
+  const [index, setIndex] = useState(0)
   useEffect(() => {
     setUsers(data)
   }, [])
 
+  function onPrevPerson() {
+    if (index === 0) {
+      setIndex(users.length - 1)
+    } else {
+      setIndex(index - 1)
+    }
+  }
+
   if (!users) return <div>Loading...</div>
   const { id, name, job, image, text } = users
+
   return (
     <main>
       <article className='review'>
@@ -20,7 +29,7 @@ const App = () => {
         <p className='job'>{users[index].job}</p>
         <p className='info'>{users[index].info}</p>
         <div className="btn-container">
-          <button className='prev-btn' >
+          <button onClick={onPrevPerson} className='prev-btn' >
             <FaChevronLeft />
           </button>
           <button className='next-btn' >
