bbb89d1640b6ca0b321bfae9bb7244b33effbb52
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-02-11 19:03:38 +0200

    Add prev and next buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react'
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 import data from './data'
 const App = () => {
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState(null)
   const [index, setIndex] = [0]
   useEffect(() => {
     setUsers(data)
   }, [])
+
   if (!users) return <div>Loading...</div>
   const { id, name, job, image, text } = users
   return (
@@ -17,6 +19,14 @@ const App = () => {
         <h4 className='author'>{users[index].name}</h4>
         <p className='job'>{users[index].job}</p>
         <p className='info'>{users[index].info}</p>
+        <div className="btn-container">
+          <button className='prev-btn' >
+            <FaChevronLeft />
+          </button>
+          <button className='next-btn' >
+            <FaChevronRight />
+          </button>
+        </div>
       </article>
     </main>
   )
