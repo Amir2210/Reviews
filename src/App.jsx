4951e1da1003ff9a2859b69076159e5e60f54ce0
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-02-11 19:23:45 +0200

    Implement getRandomPerson func

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,22 @@ const App = () => {
     }
   }
 
-  if (!users) return <div>Loading...</div>
-  const { id, name, job, image, text } = users
+  function getRandomPerson() {
+    let randomPersonIDX = getRandomInt(0, users.length)
+    if (randomPersonIDX === index) {
+      if (randomPersonIDX === 0) randomPersonIDX = users.length - 1
+      else if (randomPersonIDX === users.length - 1) randomPersonIDX = 0
+    }
+    setIndex(randomPersonIDX)
+  }
 
+  function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min)
+    const maxFloored = Math.floor(max)
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+  }
+
+  if (!users) return <div>Loading...</div>
   return (
     <main>
       <article className='review'>
@@ -35,7 +48,7 @@ const App = () => {
         </div>
         <h4 className='author'>{users[index].name}</h4>
         <p className='job'>{users[index].job}</p>
-        <p className='info'>{users[index].info}</p>
+        <p className='info'>{users[index].text}</p>
         <div className="btn-container">
           <button onClick={onPrevPerson} className='prev-btn' >
             <FaChevronLeft />
@@ -44,6 +57,7 @@ const App = () => {
             <FaChevronRight />
           </button>
         </div>
+        <button onClick={getRandomPerson} className="btn btn-hipster">Suprise me</button>
       </article>
     </main>
   )
